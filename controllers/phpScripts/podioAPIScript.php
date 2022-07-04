2b06139e75bdfb4798e4c5be650ff2befafe597e
Kevin Hernandez <kevin.hernandez001@gmail.com>
2022-07-04 19:35:19 -0400

    first commit

---
+++ controllers/phpScripts/podioAPIScript.php
@@ -0,0 +1,31 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$shortopts  = "";
+$longopts  = array(
+    "client:",
+    "secret:",
+    "username:",
+    "password:",
+    "itemID:"           
+);
+
+//get parameter values from terminal command
+$val = getopt($shortopts, $longopts);
+
+// Podio Authentication
+Podio::setup($val["client"], $val["secret"]);
+Podio::authenticate_with_password($val["username"], $val["password"]);
+
+// GET Podio Item by ID
+$item = PodioItem::get_basic($val["itemID"]);
+
+$data = [
+    "amount" => $item->fields['amount']->values,
+    "category" => $item->fields['category']->values,
+    "status" => $item->fields['status']->values,
+    "description" => $item->fields['description']->values,
+    "project" => $item->fields['project']->values
+];
+
+echo json_encode($data);
